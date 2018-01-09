f9c5a7751651cb47bf556dcd4e8f92950b7762ce
dionataCursos <dyonathankinder@gmail.com>
2018-01-09 13:55:45 -0200

    primeiro commit

---
+++ application/views/olamundo.php
@@ -0,0 +1,17 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title><?= $mensagem ?></title>
+
+    </head>
+    <body>
+
+        <div id="container">
+            <h1><?= $mensagem ?></h1>
+        </div>
+
+    </body>
+</html>
